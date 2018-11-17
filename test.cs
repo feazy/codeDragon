cbb0d58a20107c187825833d6c151d681eab1352
ifeoluwa <ojikutuifeoluwa889@gmail.com>
2018-11-17 14:33:34 +0100

    Todays C# Class

---
+++ test.cs
@@ -0,0 +1,12 @@
+using System;
+                    
+public class Program
+{
+    public static void Main()
+    {
+    var c = 3.999 + 4.11111;
+    
+        
+        Console.WriteLine(c);
+    }
+}
